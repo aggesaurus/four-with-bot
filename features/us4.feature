3bf6120778a2fe98bb6b0e16ebf79c02e3ee953d
Setareh Nahal <Setareh@laptop-h8i029qg.learnet.se>
2019-05-09 13:55:51 +0200

    nya 4

--- features/us4.feature
+++ features/us4.feature
@@ -1,22 +1,57 @@
 Feature: I want to verify if Dum bot is a worse player than Bot by playing the game
-  Let them switch between red and yellow
 
+Scenario: Play the game between red Dum bot and yellow Bot 
 Background:
-  Given I have loaded the game-page
-      
-Scenario: Play the game between red Dum bot and yellow Bot  
+Given I have loaded the game-page
 When I choose red Dum bot and yellow as Bot
 And I fill the name for both players
 And I press play button to start the game
 Then the two bots should play automatically
 And a message should show who is is the winner
 
+Scenario: Play the game between red Dum bot and yellow Bot
+Background:
+Given I have loaded the game-page
+When I choose red Dum bot and yellow as Bot
+And I fill the name for both players
+And I press play button to start the game
+Then the two bots should play automatically
+And a message should show who is is the winner
 
-Scenario: Play the game between red Bot and yellow Dum bot
-When I choose red Bot and yellow as Dum bot
-And again I fill the name for both players
-And again I press play button to start the game
-Then again the two bots should play automatically
-And again a message should show who is is the winner
+Scenario: Play the game between red Dum bot and yellow Bot
+Background:
+Given I have loaded the game-page
+When I choose red Dum bot and yellow as Bot
+And I fill the name for both players
+And I press play button to start the game
+Then the two bots should play automatically
+And a message should show who is is the winner
+
+Scenario: Play the game between red Dum bot and yellow Bot
+Background:
+Given I have loaded the game-page
+When I choose red Dum bot and yellow as Bot
+And I fill the name for both players
+And I press play button to start the game
+Then the two bots should play automatically
+And a message should show who is is the winner
+
+Scenario: Play the game between red Dum bot and yellow Bot
+Background:
+Given I have loaded the game-page
+When I choose red Dum bot and yellow as Bot
+And I fill the name for both players
+And I press play button to start the game
+Then the two bots should play automatically
+And a message should show who is is the winner
+
 And I verify who is the best player
-    
+
+
+ 
+
+
+  
+
+
+
--- step-definitions/us4.js
+++ step-definitions/us4.js
@@ -45,6 +45,7 @@ module.exports = function(){
    }
      });
      this.When(/^I fill the name for both players$/, async function () {
+         x=0;
        await sleep(500 * 2);
        let inputFields = await $('input[placeholder="Namn (2-10 tecken)"]');
        await inputFields[0].sendKeys('StupidBot1');
@@ -73,82 +74,15 @@ module.exports = function(){
         if (getmessageWin === 'BotP1 vann'){
           i++; //räknar vinster för BotP1
         }
+        //console.log(i);
         await sleep(sleepTime * 2);
         assert.equal(getmessageWin, 'BotP1 vann', 'The yellow BotP1 should win');
         await sleep(sleepTime * 2);
       });
 
-      this.When(/^I choose red Bot and yellow as Dum bot$/, async function () {
-        let typeButtons = await $('.type-choice-btn' );
-        for(let typeButton of typeButtons){
-          await typeButton.click();
-          let choices = await $('.dropdown-menu.type-choice.show .dropdown-item');
-          for(let choice of choices){
-            let text = await choice.getText();
-     
-           if(x===2)
-           {
-                if(text === 'Bot'){
-                     x++;
-                    await choice.click();
-                    break;
-              }
-           }
-     
-           if(x===3)
-           {
-                if(text === 'Dum bot'){
-                    x++;
-                    await choice.click();
-                    break;
-                }
-           }
-     
-          }
-          await sleep(500 * 2);
-        }
-      });
-
-      this.When(/^again I fill the name for both players$/, async function () {
-        await sleep(500 * 2);
-       let inputFields = await $('input[placeholder="Namn (2-10 tecken)"]');
-       await inputFields[0].sendKeys('BotP1');
-       await sleep(500 * 2);
-       await inputFields[1].sendKeys('StupidBot1');
-       await sleep(500 * 2);
-      });
-
-      this.When(/^again I press play button to start the game$/, async function () {
-        let clickButton = await $('.begin-btn');
-      clickButton.click();
-      await sleep(14000);
-      });
-
-      this.Then(/^again the two bots should play automatically$/, async function () {
-        await sleep(14000);
-      });
-
-      this.Then(/^again a message should show who is is the winner$/, async function () {        
-        await sleep(sleepTime * 2);
-        let messageWin = await driver.findElement(by.css('body > div > main > div > div:nth-of-type(1) > h3 > span'));
-        let getmessageWin = await messageWin.getText();
-        getmessageWin = getmessageWin.slice(0,10);
-        
-        if (getmessageWin === 'BotP1 vann'){
-          i++; //räknar vinster för BotP1
-        }
-        await sleep(sleepTime * 2);
-        assert.equal(getmessageWin, 'BotP1 vann', 'The red BotP1 should win');
-        //console.log('Antal vinster för BotP1 är: ' +i)
-        await sleep(sleepTime * 2);
-      });
-
       this.Then(/^I verify who is the best player$/, async function () {
-        assert.equal(i, 2, 'The Bot player should win 2 times');
+        assert.equal(i, 5, 'The Bot player should win 5 times');
       });
 
-      
-      
-
-}
+    }
 }
