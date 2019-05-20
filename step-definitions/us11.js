0757e1e2c88d252d4cf509e0831f54cfe41906ea
Per Jakobsson <perjakobsson@tele2.se>
2019-05-20 16:39:24 +0200

    us11.feature us11.js  3ggr fungerar

--- step-definitions/us11.js
+++ step-definitions/us11.js
@@ -12,6 +12,7 @@ let myNewArray = [];
 let myArray2 = [];
 let myNewArray2 = [];
 let gamesolverDriver;
+let u = 0; //räknar vinster för solvern yellow
 
 let test = [];
 
@@ -21,22 +22,23 @@ function $$(cssSelector) {
   return $(cssSelector, gamesolverDriver);
 }
 //Tar reda på vad boten spelar i prototypen(röda) och return en siffra som kan användas för att flytta bricka i gamesolver
-async function protoPlay(){
+async function protoPlay() {
+
   let theBoard = await boardToArray();
-  
-  for(let i=0; i<42; i++){ 
-    if (theBoard[i] === 'red'){
-       myArray.push(i);  
-   }
+
+  for (let i = 0; i < 42; i++) {
+    if (theBoard[i] === 'red') {
+      myArray.push(i);
+    }
   }
-  
+
   //console.log("board", myArray);
   myNewArray = [...new Set(myArray)]; //Tar bort dubbletter vilket rättar till arrayen
   //console.log("board", myNewArray);
-  
+
   let perfectNumber;
 
-  switch(myNewArray[myNewArray.length - 1]) {
+  switch (myNewArray[myNewArray.length - 1]) {
     case 0: case 7: case 14: case 21: case 28: case 35:
       perfectNumber = 4;
       break;
@@ -61,77 +63,28 @@ async function protoPlay(){
     default:
       console.log('Somethings wrong');
   }
- 
+
   return perfectNumber; //används i solver för att klicka vad boten gör
 }
 
-//Tar reda på vad boten(gula) spelar i solvern och return en siffra som kan användas för att flytta bricka i gamesolver
-/*async function solverPlay(){
-    let theBoard2 = await arrayFromSolver();
-    let x=0;
-    for(let i=0; i<42; i++){ 
-      if (theBoard2[i] === 'yellow' && i===x+1) {
-         myArray2.push(i); 
-         x++;
-     }
-    }
-    
-    console.log("nyboard", myArray2);
-    myNewArray2 = [...new Set(myArray2)]; //Tar bort dubbletter vilket rättar till arrayen
-    console.log("nyboard", myNewArray2); 
 
 
-//Detta är bara ett test
-   
-    
-    /*let perfectNumber2;
-  
-    switch(myNewArray2[myNewArray2.length - 1]) {  
-      case 0: case 7: case 14: case 21: case 28: case 35:
-        perfectNumber2 = 0;
-        break;
-      case 1: case 8: case 15: case 22: case 29: case 36:
-        perfectNumber2 = 1;
-        break;
-      case 2: case 9: case 16: case 23: case 30: case 37:
-        perfectNumber2 = 2;
-        break;
-      case 3: case 10: case 17: case 24: case 31: case 38:
-        perfectNumber2 = 3;
-        break;
-      case 4: case 11: case 18: case 25: case 32: case 39:
-        perfectNumber2 = 4;
-        break;
-      case 5: case 12: case 19: case 26: case 33: case 40:
-        perfectNumber2 = 5;
-        break;
-      case 6: case 13: case 20: case 27: case 34: case 41:
-        perfectNumber2 = 6;
-        break;
-      default:
-        console.log('Somethings wrong');
-    }
-   
-    return perfectNumber2; //används i solver för att klicka vad boten gör
-  } 
-} */
 
-
-async function boardToArray(){
+async function boardToArray() {
   let boardArray = [];
   let slots = await $('.slot'); // 42 slots
   let count = 0;
-  for(let slot of slots){
+  for (let slot of slots) {
     let cssClass = await slot.getAttribute('class');
     let color = 'empty';
-    if(cssClass.includes('red')){ color = 'red'; }
-    if(cssClass.includes('yellow')){ color = 'yellow'; }
+    if (cssClass.includes('red')) { color = 'red'; }
+    if (cssClass.includes('yellow')) { color = 'yellow'; }
     boardArray.push(color);
   }
   return boardArray;
 }
 
-async function arrayFromSolver(){
+async function arrayFromSolver() {
   let solverSearchString = await gamesolverDriver.executeScript("return window.top.location.search");
   let solverBoard = await gamesolverDriver.executeScript("return window.top.board");
   //console.log('solverSearchString', solverSearchString);
@@ -140,9 +93,9 @@ async function arrayFromSolver(){
   let last = solverSearchString.charAt(solverSearchString.length - 1);
   //console.log('solverSearchString',x);
   // let y = x.slice(0,1);
-   console.log('solverSearchString',last);
+  //console.log('solverSearchString', last);
   let send = last - 1
-   return send;
+  return send;
 }
 
 
@@ -186,7 +139,7 @@ module.exports = function () {
     let beginButton = await $('.begin-btn');
     beginButton.click();
     await sleep(sleepTime * 2);
-    
+
   });
 
   this.Then(/^the game should start$/, async function () {
@@ -197,18 +150,19 @@ module.exports = function () {
     await sleep(sleepTime * 2);
   });
 
-  
+
   // After having done a small change in funcs.js
   // so that $ can use any driver - as a second argument
   // I write my own function $$ that can be used instead of writing
   // gamesolverDriver.findElements(By.css('selector'));
-  
+
 
   this.Given(/^that we are on the gamesolver page$/, async function () {
     // creating a new driver
     gamesolverDriver = await new Builder().forBrowser('chrome').build();
     // loading the gamesolver page
     await gamesolverDriver.get('https://connect4.gamesolver.org/');
+
   });
 
   this.When(/^I choose to play as human and bot in the gamesolver$/, async function () {
@@ -226,40 +180,70 @@ module.exports = function () {
   // move so the two bots can meet automatically
   //
   // Then can we test if the perfect bot always win
-  
-  
+
+
 
   this.When(/^two bots have played until someone wins$/, async function () {
 
-    for (let i = 0; i<42; i++){
+    for (let i = 0; i < 42; i++) {
       let messageWin = await $$('#solution_header');
       let getmessageWin = await messageWin.getText();
-      getmessageWin = getmessageWin.slice(7,10);
-      console.log(getmessageWin);
+      getmessageWin = getmessageWin.slice(7, 10);
+      //console.log(getmessageWin);
       // driver.executeScript('console.log("round", ' + i + ')')
       // gamesolverDriver.executeScript('console.log("round", ' + i + ')')
-      if (getmessageWin === 'won'){
+      if (getmessageWin === 'won') {
         break;
       }
-      
+
       await sleep(sleepTime * 2);
       let myNumber = await protoPlay(); //kallar på min protoPlay funktionen
       await sleep(sleepTime * 2);
       let newBoard = await $$('.board');
       await newBoard[myNumber].click();
       await sleep(sleepTime * 2);
-      // let yButton = await $$('#player_2');
-      // await yButton.click();
+
       // await sleep(sleepTime * 4);
       //let solverNumber = await solverPlay(); //kallar på min solverPlay funktionen
       let solverBoard = await $('.slot');
       let solverNumber = await arrayFromSolver()
-      await solverBoard[solverNumber].click();  
+      await solverBoard[solverNumber].click();
       await sleep(sleepTime * 2);
       // myNumber = await protoPlay(); //kallar på min protoPlay funktionen
       // await sleep(sleepTime * 4);
       // await newBoard[myNumber].click();
+
+      if (getmessageWin === 'Yellow won') {
+        u++; //räknar vinster för Yellow
+
+      }
+
     }
+
+  });
+
+  this.Then(/^the gamesolver bot should always win$/, async function () {
+
+    let messageWin = await $$('#solution_header');
+    let getmessageWin = await messageWin.getText();
+    getmessageWin = getmessageWin.slice(0, 10);
+    //console.log(getmessageWin);
+    assert.equal(getmessageWin, 'Yellow won', 'Yellow player did not win')
+
+  });
+
+  this.Then(/^the games refreshed$/, async function () {
+    let sleepTime = 500;
+    //let raknareRed = -1;
+    myArray = [];
+    myNewArray = [];
+    myArray2 = [];
+    myNewArray2 = [];
+    //let gamesolverDriver;
+    u = 0; //räknar vinster för solvern yellow
+    test = [];
+    //await driver.refresh();
+    //await gamesolverDriver.refresh();
 
   });
 
